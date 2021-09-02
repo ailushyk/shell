11629887b8a66ef29f057c498da4bc824275414f
Alex Ilushyk <ailushyk@gmail.com>
2021-09-02 23:24:10 +0200

    install chakra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { DelayedFallback } from './components/AppLoader/DelayedFallback';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const DashboardPage = React.lazy(() => {
   return Promise.all([
     import('dashboard/DashboardPage'),
-    new Promise((resolve) => setTimeout(resolve, 500)),
+    new Promise((resolve) => setTimeout(resolve, 1500)),
   ]).then(([moduleExports]) => moduleExports);
 });
 // const App1Page = React.lazy(() => import('app1/App1Page'));
@@ -14,16 +15,18 @@ const NotFound = React.lazy(() => import('ui/NotFound'));
 
 const App = () => {
   return (
-    <Suspense fallback={<DelayedFallback />}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<DashboardPage />} />
-          {/*<Route path="/app1" element={<App1Page />} />*/}
-          <Route path="app2" element={<App2Page />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </Suspense>
+    <ChakraProvider>
+      <Suspense fallback={<DelayedFallback />}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<DashboardPage />} />
+            {/*<Route path="/app1" element={<App1Page />} />*/}
+            <Route path="app2" element={<App2Page />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
+    </ChakraProvider>
   );
 };
 
